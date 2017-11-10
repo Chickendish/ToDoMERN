29f36b360009416a0762be0ac7380592004deb43
Bill Donovan <williamcdonovan@gmail.com>
2017-11-10 11:14:03 -0700

    first successful build with webpack

--- public/bundle.js
+++ public/bundle.js
@@ -65,9 +65,10 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-throw new Error("Module build failed: Error: Couldn't find preset \"es2015\" relative to directory \"/Users/williamdonovan/Documents/React/ToDoMERN\"\n    at /Users/williamdonovan/Documents/React/ToDoMERN/node_modules/babel-core/lib/transformation/file/options/option-manager.js:293:19\n    at Array.map (native)\n    at OptionManager.resolvePresets (/Users/williamdonovan/Documents/React/ToDoMERN/node_modules/babel-core/lib/transformation/file/options/option-manager.js:275:20)\n    at OptionManager.mergePresets (/Users/williamdonovan/Documents/React/ToDoMERN/node_modules/babel-core/lib/transformation/file/options/option-manager.js:264:10)\n    at OptionManager.mergeOptions (/Users/williamdonovan/Documents/React/ToDoMERN/node_modules/babel-core/lib/transformation/file/options/option-manager.js:249:14)\n    at OptionManager.init (/Users/williamdonovan/Documents/React/ToDoMERN/node_modules/babel-core/lib/transformation/file/options/option-manager.js:368:12)\n    at File.initOptions (/Users/williamdonovan/Documents/React/ToDoMERN/node_modules/babel-core/lib/transformation/file/index.js:212:65)\n    at new File (/Users/williamdonovan/Documents/React/ToDoMERN/node_modules/babel-core/lib/transformation/file/index.js:135:24)\n    at Pipeline.transform (/Users/williamdonovan/Documents/React/ToDoMERN/node_modules/babel-core/lib/transformation/pipeline.js:46:16)\n    at transpile (/Users/williamdonovan/Documents/React/ToDoMERN/node_modules/babel-loader/lib/index.js:50:20)\n    at Object.module.exports (/Users/williamdonovan/Documents/React/ToDoMERN/node_modules/babel-loader/lib/index.js:175:20)");
+"use strict";
+
 
 /***/ })
 /******/ ]);
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   rules: [
     { test: /\.js$/, 
       exclude: /node_modules/, 
-      loader: "babel-loader" }
+      use: "babel-loader" }
   ]
 }
 };
