266a6fc811c8ca0674adf0aa238f15c2c34fa028
Bill Donovan <williamcdonovan@gmail.com>
2017-11-14 14:23:26 -0700

    added new file

---
+++ src/app.js
@@ -0,0 +1,8 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// Include the main Parent Component
+var Parent = require("./components/Parent");
+
+// This code here allows us to render our main component (in this case Parent)
+ReactDOM.render(<Parent />, document.getElementById("app"));
